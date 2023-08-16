8dcc840d37a02a40a30b802b925b19498a8adfaf
SoarinSkySagar <sagarrana.pvt@gmail.com>
2023-08-16 13:10:25 +0530

    created webpage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
 
-function App() {
+export default function App() {
+  function Submit(e) {
+    const formEle = document.querySelector("form");
+    const formDatab = new FormData(formEle);
+    fetch(
+      "https://script.google.com/macros/s/AKfycbxfi01kW0oJKzwoOlT8cft28Fl23Gm5tk4AnDodmehTv_QPt01Qiq7z5iwazEFOOZ1v3g/exec",
+      {
+        method: "POST",
+        body: formDatab
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Waitlist for "assignmate"</h1>
+      <h2>
+        We are working on the platform right now, until then, if you are
+        interested in trying our platform once it comes out, join the waitlist!!
+        It's FREE
+      </h2>
+      <div>
+        <form className="form" onSubmit={(e) => Submit(e)}>
+          <input placeholder="Your Name" name="Name" type="text" />
+          <input placeholder="Your Email" name="Email" type="text" />
+          <input placeholder="Your Message" name="Message" type="text" />
+          <input name="Name" type="submit" />
+        </form>
+      </div>
     </div>
   );
 }
-
-export default App;
